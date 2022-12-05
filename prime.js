f14bd38d8853e97351dcdd47ba31aa065723abf5
unknown <vikastiwari26071997@gmail.com>
2022-12-05 10:44:44 -0800

    prime_reg

---
+++ prime.js
@@ -0,0 +1,17 @@
+for(let i=1;i<=20;i++){
+  
+let num= i;
+
+let count=0;
+for(let j= 1; j>=num; j++ ){
+
+  if(num%j==0){
+    count++
+  }
+}
+
+
+if(count==2){
+  console.log(i,"is a prime number");
+} 
+}
